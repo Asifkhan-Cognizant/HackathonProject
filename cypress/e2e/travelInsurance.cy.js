b22e50d0e77877fa839342b7e76aafbac7989a06
Asifkhan-Cognizant <Asifkhan.S@cognizant.com>
2025-06-03 16:16:43 +0530

    Last commit

--- cypress/e2e/travelInsurance.cy.js
+++ cypress/e2e/travelInsurance.cy.js
@@ -1,9 +1,11 @@
-import travelForm from "../support/Pages/travelForm";
 
+ 
+import travelForm from "../support/Pages/travelForm";
+ 
 describe("Hackathon Project", () => {
   let googleVisit, googleSearch, travelVisit;
   const TravelForm = new travelForm();
-
+ 
   before(() => {
     cy.fixture("travelForm")
       .then((data) => {
@@ -19,40 +21,40 @@ describe("Hackathon Project", () => {
         TravelForm.TravelVisit(travelVisit);
       });
   });
-
+ 
   beforeEach(() => {
     Cypress.on("uncaught:exception", () => {
       return false;
     });
   });
-
+ 
   // it("To Verify seamless navigation to Travel Insurance page.", () => {
   //    // cy.visit('https://www.policybazaar.com/?utm_source=google_brand&utm_medium=ppc&utm_term=Policy%20bazaar&utm_campaign=Brand_Policy_Bazaar_Exact00Brand-policy_bazaar&gad_source=1&gad_campaignid=2055878099&gbraid=0AAAAADwVZjK6vYTCZSwNsmpT9ulrjeQIg&gclid=EAIaIQobChMIuIP5wu7RjQMVY9QWBR3loRy3EAAYASAAEgKbAvD_BwE')
-
+ 
   //   TravelForm.home(googleVisit, googleSearch,travelVisit);
   //   TravelForm.TravelVisit(travelVisit);
   // });
-
+ 
   it("To Verify accurate selection of a European country and dummy travel dates.", () => {
     TravelForm.CountryAndDateSelection(travelVisit);
   });
-
+ 
   it('To Verify error message for selecting past travel start dates.',()=>{    
     TravelForm.checkInvalidDate()
   })
-  
+ 
   it('To Verify error message for selecting travel end date before start date.',()=>{    
     TravelForm.checkEndDateselection()
   })
-
+ 
   it("To Verify successful selection of 2 travelers (Ages 22, 21).", () => {
     TravelForm.Passenger(travelVisit);
   });
-
+ 
   it("To Verify display of enquiry contact information", () => {
     TravelForm.CallDetails();
   });
-
+ 
   it("To Verify behavior when no destination is selected but user attempts to proceed.", () => {
     TravelForm.Date();
     TravelForm.Passenger();
@@ -60,7 +62,7 @@ describe("Hackathon Project", () => {
     TravelForm.Result();
     TravelForm.CountryErrorCheck();
   });
-
+ 
   it("To verify behaviour when no traveller information is selected but user attempts to proceed", () => {
     TravelForm.Date();
     TravelForm.Country();
@@ -69,3 +71,6 @@ describe("Hackathon Project", () => {
     TravelForm.TravellerErrorCheck();
   });
 });
+ 
+ 
+ 
